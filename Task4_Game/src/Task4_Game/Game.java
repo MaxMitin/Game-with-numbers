b2e638947a74e1eaf8f25b21f48bcbfe08603618
Maksim Mitin <141111112+MaxMitin@users.noreply.github.com>
2024-02-06 06:50:15 +0300

    Update Game.java

--- Task4_Game/src/Task4_Game/Game.java
+++ Task4_Game/src/Task4_Game/Game.java
@@ -2,18 +2,14 @@ package Task4_Game;
 
 import java.util.Scanner;
 
-public class Game {public static void main(String[] arg) {
-		
+public class Game {public static void main(String[] arg) {	
 	
 	int a = firstNumber();
-	if (a != 0) {
-		int b = 9;
-		int c = thirdNumber(a);
-		System.out.println("Итоговое трёхзначное число: " + a + b + c);
+	int b = 9;
+	int c = thirdNumber(a);
+	System.out.println("Итоговое трёхзначное число: " + a + b + c);
 	}
-	else {System.out.println("Вы ввели не то число!");}
 	
-}
 	private static int firstNumber() {
 			
 		System.out.println("Введите первую цифру трехзначного числа (в диапазоне от нуля до девяти)!");
@@ -28,8 +24,6 @@ public class Game {public static void main(String[] arg) {
 		int b = 9; 
 		int c = b-a;
 		
-		return c;
-		
+		return c;	
 	}
-
 }
